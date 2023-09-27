81d5a20a6baae328fa87c72e1cdc50af06b97c65
Soham Dasgupta <sdasgupta@leaseaccelerator.com>
2023-09-27 15:28:12 +0530

    ci: remove build output checks

--- polygon.ps1
+++ polygon.ps1
@@ -1,7 +1,12 @@
-Remove-Item -Path .\build\* -Recurse -Force
-Ahk2Exe.exe /in polygon.ahk /out .\build\polygon-x64.exe /icon logo.ico /base "$HOME\.scoop\apps\autohotkey\current\v2\AutoHotkey64.exe"
-Ahk2Exe.exe /in polygon.ahk /out .\build\polygon-x86.exe /icon logo.ico /base "$HOME\.scoop\apps\autohotkey\current\v2\AutoHotkey32.exe"
-candle.exe .\polygon.wxs -out .\build\polygon-x86.wixobj -dVersion="0.0.0" -arch x86
-candle.exe .\polygon.wxs -out .\build\polygon-x64.wixobj -dVersion="0.0.0" -arch x64
-light.exe .\build\polygon-x86.wixobj -ext WixUIExtension -out .\build\polygon-x86.msi
-light.exe .\build\polygon-x64.wixobj -ext WixUIExtension -out .\build\polygon-x64.msi
+$cwd = (Get-Location).Path;
+
+Remove-Item -Path "$cwd\build\*" -Recurse -Force;
+
+Ahk2Exe.exe /in polygon.ahk /out "$cwd\build\polygon-x64.exe" /icon logo.ico /base "$HOME\.scoop\apps\autohotkey\current\v2\AutoHotkey64.exe";
+Ahk2Exe.exe /in polygon.ahk /out "$cwd\build\polygon-x86.exe" /icon logo.ico /base "$HOME\.scoop\apps\autohotkey\current\v2\AutoHotkey32.exe";
+
+candle.exe .\polygon.wxs -out "$cwd\build\polygon-x86.wixobj" -dVersion="0.0.0" -arch x86;
+candle.exe .\polygon.wxs -out "$cwd\build\polygon-x64.wixobj" -dVersion="0.0.0" -arch x64;
+
+light.exe "$cwd\build\polygon-x86.wixobj" -ext WixUIExtension -out "$cwd\build\polygon-x86.msi";
+light.exe "$cwd\build\polygon-x64.wixobj" -ext WixUIExtension -out "$cwd\build\polygon-x64.msi";
